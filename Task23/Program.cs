2a84b4835d12a440e9d7a0c75ab10898e137b86c
Elena Potapkina <epotapkina.1@gmail.com>
2022-11-03 17:47:16 +0300

    add task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Square (int x)
+
+{
+    if (x>0)
+    {
+    for (int i = 1; i <= x; i++)
+    {
+        System.Console.WriteLine($"{i} -> {i*i*i}");
+    }
+    }
+    else
+    {
+        System.Console.WriteLine("Введите значение больше 0");
+    }
+}
+
+Square (num);
