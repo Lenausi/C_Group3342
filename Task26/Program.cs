5613b6ff636861ab9dd21228688ef8fa5be2339b
Elena Potapkina <epotapkina.1@gmail.com>
2022-11-11 13:36:47 +0300

    add task 26

---
+++ Task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int NumDig (int number)
+{
+int count  = 0;
+ while (number>0)
+    {
+        number = number/10;
+        count ++;
+    }
+    return count > 0 ? count :1 ;
+}
+
+int result = NumDig (num);
+System.Console.WriteLine($"Кол-во цифр в числе {num} равно {result}");
