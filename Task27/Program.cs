6d2c730c8b7a508f8997c6c7293eea3d227078aa
Elena Potapkina <epotapkina.1@gmail.com>
2022-11-15 18:44:35 +0300

    add task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumofDig (int a)
+{
+    int sum = default;
+    int numofdig = Convert.ToString(a).Length;
+
+    for (int i = 0; i < numofdig; i++)
+    {
+        
+        sum = sum +a%10;
+        a = a/10;
+       
+    }
+
+  return sum;
+}
+
+int res = SumofDig (num);
+System.Console.WriteLine($"{res}");
